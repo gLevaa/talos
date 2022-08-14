58ce28d90aefdb4129b0a2e68a026fb1760d3983
gLevaa <lunasityboi@gmail.com>
2022-08-14 19:01:39 +1200

    Generalise await request/response functions

--- server/src/connections/RequestManager.java
+++ server/src/connections/RequestManager.java
@@ -17,34 +17,58 @@ public class RequestManager {
         this.output = output;
     }
 
-    public JSONObject awaitInitRequest() {
+    public JSONObject awaitRequest(String type) {
         try {
-            JSONObject initRequest = new JSONObject(input.readLine());
-
-            if (initRequest.has("request_type") && initRequest.has("who")) {
-                return initRequest;
-            } else {
-                serveResponse("503"); // REQUEST.FAILED_INIT_READ
-                return null;
-            }
+            return switch (type) {
+                case "init" -> awaitInitRequest();
+                case "auth_response" -> awaitAuthRequest();
+                default -> null;
+            };
         } catch (SocketTimeoutException e) {
             // 201 - TIMEOUT.INIT
+            // Response redundant, timed out
             e.printStackTrace();
+
             return null;
         } catch (IOException e) {
-            serveResponse("303"); // SERVER.IO_ERROR
+            serveFinalResponse("303"); // SERVER.IO_ERROR
             e.printStackTrace();
 
             return null;
         } catch (JSONException e) {
-            serveResponse("503"); // REQUEST.FAILED_INIT_READ
-            e.printStackTrace();
+            switch (type) {
+                case "init" -> serveFinalResponse("503"); // REQUEST.FAILED_INIT_READ
+                case "auth_response" -> serveFinalResponse("504"); // REQUEST.FAILED_AUTH_READ
+            }
 
+            e.printStackTrace();
             return null;
         }
     }
 
-    public void serveResponse(String code) {
+    private JSONObject awaitInitRequest() throws IOException, SocketTimeoutException, JSONException {
+        JSONObject initRequest = new JSONObject(input.readLine());
+
+        if (initRequest.has("request_type") && initRequest.has("who")) {
+            return initRequest;
+        } else {
+            serveFinalResponse("503"); // REQUEST.FAILED_INIT_READ
+            return null;
+        }
+    }
+
+    private JSONObject awaitAuthRequest()  throws IOException, SocketTimeoutException, JSONException {
+        JSONObject authResponse = new JSONObject(input.readLine());
+
+        if (authResponse.has("request_type") && (authResponse.has("key") || authResponse.has("session_id"))) {
+            return authResponse;
+        } else {
+            serveFinalResponse("504"); // REQUEST.FAILED_AUTH_READ
+            return null;
+        }
+    }
+
+    public void serveFinalResponse(String code) {
         JSONObject response = new JSONObject();
         response.put("request_type", "server_response");
         response.put("code", code);
