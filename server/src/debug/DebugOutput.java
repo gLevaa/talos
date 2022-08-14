2bcdafe5cda8b42faf92c0e70af9dfe2c8f2a158
gLevaa <lunasityboi@gmail.com>
2022-08-14 17:32:13 +1200

    Add prefix, implementation work in progress

--- server/src/debug/DebugOutput.java
+++ server/src/debug/DebugOutput.java
@@ -1,4 +1,10 @@
 package debug;
 
+import java.time.ZonedDateTime;
+
 public class DebugOutput {
+    private static String preparePrefix() {
+        ZonedDateTime dateTime = ZonedDateTime.now();
+        return "[Server @" + dateTime.getHour() + ":" + dateTime.getMinute() + ":" + dateTime.getSecond() + "] ";
+    }
 }
