3b8d2ac06b5bb845b73b363826db3dffdc2e7f08
gLevaa <lunasityboi@gmail.com>
2022-08-14 18:40:01 +1200

    Add synchronize block and move getIp to method

--- server/src/connections/ConnectionMonitor.java
+++ server/src/connections/ConnectionMonitor.java
@@ -1,29 +1,33 @@
 package connections;
 
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 
 public class ConnectionMonitor {
     private static final HashMap<String, ArrayList<String>> connectionCache = new HashMap<>();
 
     public static void logConnection(Socket socket) {
-        InetSocketAddress socketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-        String ipAddress = socketAddress.getAddress().toString().split("/")[1];
+        String ipAddress = getIpAddress(socket);
 
         ArrayList<String> entry = new ArrayList<>();
         entry.add(0, ""); entry.add(1, "");
 
-        if (connectionCache.containsKey(ipAddress)) {
-            entry = connectionCache.get(ipAddress);
+        synchronized (connectionCache) {
+            if (connectionCache.containsKey(ipAddress)) {
+                entry = connectionCache.get(ipAddress);
+            }
+
+            entry.set(0, String.valueOf(ZonedDateTime.now()));
+
+            connectionCache.put(ipAddress, entry);
         }
+    }
 
-        entry.set(0, String.valueOf(ZonedDateTime.now()));
-
-        connectionCache.put(ipAddress, entry);
+    public static String getIpAddress(Socket socket) {
+        InetSocketAddress socketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+        return socketAddress.getAddress().toString().split("/")[1];
     }
 }
