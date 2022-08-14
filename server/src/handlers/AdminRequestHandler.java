67a3784aab7ed6a0e2730cab3ded7857635ff5a7
gLevaa <lunasityboi@gmail.com>
2022-08-14 18:42:49 +1200

    Expand on framework, work in progress

--- server/src/handlers/AdminRequestHandler.java
+++ server/src/handlers/AdminRequestHandler.java
@@ -5,19 +5,23 @@ import util.PageFrontier;
 
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 public class AdminRequestHandler {
     private final BufferedReader input;
     private final PrintWriter output;
     private final RequestManager requestManager;
     private final PageFrontier pageFrontier;
+    private final Socket connectionSocket;
 
     public AdminRequestHandler(BufferedReader input, PrintWriter output,
-                                RequestManager requestManager, PageFrontier pageFrontier) {
+                                RequestManager requestManager, PageFrontier pageFrontier,
+                                Socket connectionSocket) {
         this.input = input;
         this.output = output;
         this.requestManager = requestManager;
         this.pageFrontier = pageFrontier;
+        this.connectionSocket = connectionSocket;
     }
 
     public void run() {
