cc1631ef054f62af99d6a4d984fbc18b5e7b0ddf
gLevaa <lunasityboi@gmail.com>
2022-08-14 17:27:03 +1200

    Add connection monitor base, work in progress

--- server/src/connections/ConnectionMonitor.java
+++ server/src/connections/ConnectionMonitor.java
@@ -1,4 +1,29 @@
 package connections;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 public class ConnectionMonitor {
+    private static final HashMap<String, ArrayList<String>> connectionCache = new HashMap<>();
+
+    public static void logConnection(Socket socket) {
+        InetSocketAddress socketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+        String ipAddress = socketAddress.getAddress().toString().split("/")[1];
+
+        ArrayList<String> entry = new ArrayList<>();
+        entry.add(0, ""); entry.add(1, "");
+
+        if (connectionCache.containsKey(ipAddress)) {
+            entry = connectionCache.get(ipAddress);
+        }
+
+        entry.set(0, String.valueOf(ZonedDateTime.now()));
+
+        connectionCache.put(ipAddress, entry);
+    }
 }
