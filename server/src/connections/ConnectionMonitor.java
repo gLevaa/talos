5aec23548831d76b169287998cf4443fddb9498c
gLevaa <lunasityboi@gmail.com>
2022-08-22 14:54:20 +1200

    Removed session ID idea, simply log IP and time

--- server/src/connections/ConnectionMonitor.java
+++ server/src/connections/ConnectionMonitor.java
@@ -3,21 +3,16 @@ package connections;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.time.LocalTime;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class ConnectionMonitor {
-    private static final HashMap<String, List<String>> connectionCache = new HashMap<>();
+    private static final HashMap<String, LocalTime> connectionCache = new HashMap<>();
 
     public static void logConnection(Socket socket) {
         String ipAddress = getIpAddress(socket);
 
         synchronized (connectionCache) {
-            List<String> entry = connectionCache.getOrDefault(ipAddress, Arrays.asList("", "NA"));
-            entry.set(0, String.valueOf(LocalTime.now()));
-
-            connectionCache.put(ipAddress, entry);
+            connectionCache.put(ipAddress, LocalTime.now());
         }
     }
 
